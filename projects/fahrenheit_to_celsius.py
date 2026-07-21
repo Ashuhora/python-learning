9f24d6c6891aea73d55f100709cca9d9c83ac187
Ashu Hora <ashuhora777@gmail.com>
2026-07-21 16:27:34 -0600

    Added Fahrenheit to Celsius converter

---
+++ projects/fahrenheit_to_celsius.py
@@ -0,0 +1,10 @@
+# Fahrenheit to Celsius Converter
+# This program converts Fahrenheit to Celsius.
+
+fahrenheit = float(input("Enter the temperature in Fahrenheit: "))
+
+celsius = (fahrenheit - 32) * 5 / 9
+
+
+
+print(f"Temperature in Celsius: {celsius:.2f}°C")
