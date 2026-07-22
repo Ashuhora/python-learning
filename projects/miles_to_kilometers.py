164a3f5c21c4be3bb2b0a29b2baf41ad5888bc9a
Ashu Hora <ashuhora777@gmail.com>
2026-07-22 14:07:46 -0600

    Added miles to kilometers converter

---
+++ projects/miles_to_kilometers.py
@@ -0,0 +1,8 @@
+# Miles to Kilometers Converter
+
+
+miles = float(input("Enter distance in miles: "))
+
+kilometers = miles * 1.60934
+
+print(f"{miles:.2f} miles is equal to {kilometers:.2f} kilometers.")
