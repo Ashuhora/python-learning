00d6c0ab121c7f40ac0db44670aeae32f1ea0956
Ashu Hora <ashuhora777@gmail.com>
2026-07-24 09:22:18 -0600

    Added list examples, append, index, and pop

---
+++ projects/lists/append_insert.py
@@ -0,0 +1,26 @@
+# This is an empty list example
+
+todo_list = []
+
+print("Your to-do list:", todo_list)
+
+# This is append() example
+
+todo_list = []
+
+todo_list.append("buy groceries")
+todo_list.append("finish homework")
+todo_list.append("call mom")
+print("updated list:", todo_list)
+
+# This is insert() example
+
+todo_list = []
+
+todo_list.append("buy groceries")
+todo_list.append("finish homework")
+todo_list.append("call mom")
+
+todo_list.insert(1, "pay bills")
+
+print(todo_list)
---
+++ projects/lists/index_slicing.py
@@ -0,0 +1,12 @@
+# This is Indexing and Slicing example
+
+todo_list = []
+
+todo_list.append("Buy groceries")
+todo_list.append("Finish homework")
+todo_list.append("Call mom")
+
+todo_list.insert(1, "pay bills")
+
+print("first task:", todo_list[0])
+print("last two tasks:", todo_list[-2:])
