bb2e7031f255921e0ee4ecd902ffed2e63adb9c5
Ashu Hora <ashuhora777@gmail.com>
2026-07-24 10:27:37 -0600

    Complete Python modules examples

--- projects/modules/modules.py
+++ projects/modules/modules.py
@@ -16,3 +16,31 @@ if coin == 1:
     print("Heads")
 else:
     print("Tails")
+
+# Multiple Modules
+
+import random
+import math
+
+print(random.randint(1, 6))
+print(math.sqrt(64))
+
+# Module Alias
+# as gives the module a shorter name.
+
+import random as rng
+
+print(rng.randint(1, 10))
+
+# Importing only One Function
+
+from random import randint
+
+print(randint(1, 10))
+
+# Random Decimal
+# This random() returns a decimal between 0.00 <= 1 (1 is not included)
+
+import random
+
+print(random.random())
