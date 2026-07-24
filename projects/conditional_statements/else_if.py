1b4f8c05d8b5b8815a5fe50e2d10804634929027
Ashu Hora <ashuhora777@gmail.com>
2026-07-24 10:01:45 -0600

    Added if and else-if statement examples

---
+++ projects/conditional_statements/else_if.py
@@ -0,0 +1,31 @@
+# This is else if statement
+
+score = int(input("Enter your exam score:"))
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
+
+
+# else if with float and whole (int) number
+
+score = float(input("Enter your exam score: "))
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
+
--- projects/conditional_statements/if_statement.py
+++ projects/conditional_statements/if_statement.py
@@ -1,11 +1,11 @@
-# If Statement
+# If Statement example 1
 
 grade = input ("Enter your grade letter:").upper()
 
 if grade == "F":
     print("You have failed the class.")
 
-# If Statement
+# If Statement example 2
 
 grade = input("Enter your grade letter:").upper()
 
@@ -15,6 +15,16 @@ if grade == "F":
 
 print("Thank You!")
 
-# if 
+# If Statement example 3
+
+# If Statement
+
+temperature = int(input("Enter the temperature: "))
+
+if temperature > 90:
+    print("It is very hot outside.")
+    print("Drink plenty of water.")
+
+print("Have a good day!")
 
 
